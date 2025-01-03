d1891342e5cf6117a3da1ad74027f00239235ba5
Haton Petr <phaton@vlslinmgmt11.pmb.cz>
2025-01-03 14:41:35 +0100

    Cteni parametru ze souboru

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -13,7 +14,12 @@ import (
 	gitlab "github.com/Cloud-for-You/devops-cli/cmd/gitlab"
 )
 
-var Debug bool
+var (
+	Debug      bool
+	configFile string
+	Command    string
+	Flags      map[string]string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -21,15 +27,21 @@ var rootCmd = &cobra.Command{
 	Short:                 "Client for DEVOPS tools management",
 	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Run `devops-cli --help` for usage.")
+		if Command != "" {
+			executeCommandFromConfig(cmd)
+		} else {
+			fmt.Println("Run `devops-cli --help` for usage.")
+		}
 	},
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	rootCmd.AddCommand(gitlab.GitlabCmd)
 
 	rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "Display debugging output in the console. (default: false)")
-	viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug"))
+	rootCmd.Flags().StringVar(&configFile, "config", "", "Configuration file from command and flags.")
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -38,5 +50,40 @@ func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	if configFile != "" {
+		viper.SetConfigFile(configFile)
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file: %v\n", err)
+		return
+	}
+
+	Command = viper.GetString("command")
+	Flags = viper.GetStringMapString("flags")
+	fmt.Println(Command)
+}
+
+func executeCommandFromConfig(cmd *cobra.Command) {
+	parts := strings.Split(Command, "/")
+
+	// Pripravime argumenty pro prikaz
+	args := append([]string{parts[0]}, parts[1:]...)
+
+	for flag, value := range Flags {
+		args = append(args, fmt.Sprintf("--%s=\"%s\"", flag, value))
+	}
+
+	// Nastavime argumenty prikazu
+	cmd.SetArgs(args)
+	fmt.Printf("Executing command: %s\n", strings.Join(args, " "))
+
+	// Spustime prikaz s argumenty
+	if err := cmd.Execute(); err != nil {
+		fmt.Printf("Error executing command: %v\n", err)
 	}
 }
